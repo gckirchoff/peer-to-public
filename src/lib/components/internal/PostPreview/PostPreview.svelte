9dabe9f6a40421e68a1e639f2ee336bca62ac854
gckirchoff <gckirchoff@gmail.com>
2023-08-15 17:09:06 -0400

    Using tags

--- src/lib/components/internal/PostPreview/PostPreview.svelte
+++ src/lib/components/internal/PostPreview/PostPreview.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type Post from '$lib/types/post';
 	import { H4, SubTitle1, SubTitle2 } from '$lib/components/internal/typography';
+	import CategoryTag from '../CategoryTag/CategoryTag.svelte';
+	import CategoryTagsList from '../CategoryTagsList/CategoryTagsList.svelte';
 
 	export let post: Post;
 </script>
@@ -13,6 +15,11 @@
 				<H4>
 					{post.title}
 				</H4>
+				<CategoryTagsList>
+					{#each post.categories as category}
+						<CategoryTag {category} />
+					{/each}
+				</CategoryTagsList>
 				<SubTitle1>{post.description}</SubTitle1>
 				<SubTitle2>{post.date}</SubTitle2>
 			</div>
@@ -24,12 +31,12 @@
 	.card {
 		display: grid;
 		grid-template-columns: 1fr 3fr;
-        gap: var(--spacing-32);
+		gap: var(--spacing-32);
 
 		img {
 			object-fit: cover;
-            min-height: 100%;
-            width: 100%;
+			min-height: 100%;
+			width: 100%;
 		}
 	}
 </style>
