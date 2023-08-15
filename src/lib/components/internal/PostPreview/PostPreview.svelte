fe1e9340691f16584fc2ca1e3669ebc14342bac5
gckirchoff <gckirchoff@gmail.com>
2023-08-15 16:23:11 -0400

    Styling posts list

---
+++ src/lib/components/internal/PostPreview/PostPreview.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type Post from '$lib/types/post';
+	import { H4, SubTitle1, SubTitle2 } from '$lib/components/internal/typography';
+
+	export let post: Post;
+</script>
+
+<li>
+	<article>
+		<a href="/posts/{post.slug}" class="card">
+			<img src="/postImages/{post.coverImage}" alt={post.title} />
+			<div>
+				<H4>
+					{post.title}
+				</H4>
+				<SubTitle1>{post.description}</SubTitle1>
+				<SubTitle2>{post.date}</SubTitle2>
+			</div>
+		</a>
+	</article>
+</li>
+
+<style lang="scss">
+	.card {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+        gap: var(--spacing-32);
+
+		img {
+			object-fit: cover;
+            min-height: 100%;
+            width: 100%;
+		}
+	}
+</style>
--- src/lib/components/internal/PostsList/PostsList.svelte
+++ src/lib/components/internal/PostsList/PostsList.svelte
@@ -1,31 +1,21 @@
 <script lang="ts">
 	import type Post from '$lib/types/post';
-	import H3 from '../typography/H3.svelte';
-	import SubTitle1 from '../typography/SubTitle1.svelte';
-	import SubTitle2 from '../typography/SubTitle2.svelte';
+	import PostPreview from '../PostPreview/PostPreview.svelte';
 
 	export let posts: Post[];
 </script>
 
 <ul>
 	{#each posts as post (post.title)}
-		<li>
-			<article>
-				<a href="/posts/{post.slug}">
-					<img src="/postImages/{post.coverImage}" alt={post.title} />
-					<H3>
-						{post.title}
-					</H3>
-					<SubTitle1>{post.description}</SubTitle1>
-					<SubTitle2>Created on {post.date}</SubTitle2>
-				</a>
-			</article>
-		</li>
+		<PostPreview {post} />
 	{/each}
 </ul>
 
 <style>
 	ul {
 		list-style: none;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-32)
 	}
 </style>
