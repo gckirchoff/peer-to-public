80ac29bf73599d19f0ba2f9e87cf89edca650971
gckirchoff <gckirchoff@gmail.com>
2023-08-15 17:08:44 -0400

    Added category tags and list components

---
+++ src/lib/components/internal/CategoryTag/CategoryTag.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Body1 } from '../typography';
+
+	export let category: string;
+</script>
+
+<Body1><a href="/posts/category/{category}">{category}</a></Body1>
+
+<style lang="scss">
+	a {
+		background-color: var(--clr-surface-600);
+		padding: var(--spacing-4) var(--spacing-8);
+		border-radius: var(--rounded-4);
+		color: var(--clr-text);
+
+		&:hover {
+			background-color: var(--clr-surface-700);
+			color: var(--clr-txt-neg);
+		}
+	}
+</style>
---
+++ src/lib/components/internal/CategoryTagsList/CategoryTagsList.svelte
@@ -0,0 +1,10 @@
+<ul><slot /></ul>
+
+<style lang="scss">
+	ul {
+		list-style: none;
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--spacing-8);
+	}
+</style>
