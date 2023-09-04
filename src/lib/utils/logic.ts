0782fde3e573a9a72ab64042e2e1cc88355426f5
gckirchoff <gckirchoff@gmail.com>
2023-09-04 15:42:25 -0400

    Added slugify helper

--- src/lib/utils/logic.ts
+++ src/lib/utils/logic.ts
@@ -3,3 +3,13 @@ export const ensureTargetIsArray = <T>(target: T | T[]): T[] => {
 
 	return Array.isArray(target) ? target : [target];
 };
+
+export const slugify = (str: string): string =>
+	str
+		.normalize('NFKD') // split accented characters into their base characters and diacritical marks
+		.replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+		.trim() // trim leading or trailing whitespace
+		.toLowerCase() // convert to lowercase
+		.replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+		.replace(/\s+/g, '-') // replace spaces with hyphens
+		.replace(/-+/g, '-'); // remove consecutive hyphens
