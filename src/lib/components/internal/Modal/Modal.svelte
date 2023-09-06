d08ada76b96e910e96c9d1ea9e481fa123122fe6
gckirchoff <gckirchoff@gmail.com>
2023-09-06 14:45:51 -0400

    Starting modal component

---
+++ src/lib/components/internal/Modal/Modal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let open: boolean;
+</script>
+
+<div class="modal" class:open>
+	<slot />
+</div>
+
+<style lang="scss">
+	.modal {
+		position: fixed;
+		display: none;
+	}
+
+	.open {
+		display: block;
+	}
+</style>
