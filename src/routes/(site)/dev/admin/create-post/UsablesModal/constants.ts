1355c0bdf11accbe10acd81521ca2b9c6e5374a7
gckirchoff <gckirchoff@gmail.com>
2023-09-08 16:40:26 -0400

    Adding Usable types

--- src/routes/(site)/dev/admin/create-post/UsablesModal/constants.ts
+++ src/routes/(site)/dev/admin/create-post/UsablesModal/constants.ts
@@ -1,4 +1,29 @@
+import type { Ingredients, Step } from '$lib/components/usables/RecipeCard/types';
+
 export enum UsableType {
 	RecipeCard = 'recipe-card',
 	PhotoGallery = 'photo-gallery',
 }
+
+interface BaseUsable {
+	id: string;
+	type: UsableType;
+}
+
+interface RecipeCard extends BaseUsable {
+	type: UsableType.RecipeCard;
+	title: string;
+	img: string;
+	description: string;
+	prepTime: number;
+	cookTime: number;
+	servings: number;
+	ingredients: Ingredients | Ingredients[];
+	steps: Step[];
+}
+
+interface PhotoGallery extends BaseUsable {
+	type: UsableType.PhotoGallery;
+}
+
+export type Usable = RecipeCard | PhotoGallery;
