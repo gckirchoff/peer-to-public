0eccc8161fbd1124364e73d9deb73312f7121697
gckirchoff <gckirchoff@gmail.com>
2023-09-12 06:18:53 -0400

    Added Button component

---
+++ src/lib/components/internal/Button/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let type: 'submit' | 'button' | 'reset' | null | undefined = undefined;
+	export let style: string = '';
+</script>
+
+<button on:click {type} {style}><slot /></button>
+
+<style lang="scss">
+	button {
+		background-color: var(--clr-primary-300);
+		padding: var(--spacing-4) var(--spacing-8);
+		border-radius: var(--rounded-4);
+
+		&:hover {
+			background-color: var(--clr-primary-400);
+		}
+	}
+</style>
