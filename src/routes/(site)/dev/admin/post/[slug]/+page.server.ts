a1cab25d4ba9cfe23cb32ab8c5dc39a4100c3ba8
gckirchoff <gckirchoff@gmail.com>
2023-09-20 15:17:55 -0400

    Fetch allCategories in loader

--- src/routes/(site)/dev/admin/post/[slug]/+page.server.ts
+++ src/routes/(site)/dev/admin/post/[slug]/+page.server.ts
@@ -3,16 +3,22 @@ import { redirect } from '@sveltejs/kit';
 
 import type Post from '$lib/types/post.js';
 
-export const load = async ({ params }) => {
+export const load = async ({ params, fetch }) => {
 	const { slug } = params;
 
 	try {
 		const post = await import(`../../../../../../lib/content/posts/${slug}.md`);
 		const postContent = await readFile(`src/lib/content/posts/${slug}.md`, 'utf-8');
 
+		const allPostsRes = await fetch('/api/posts/all');
+		const allPosts = (await allPostsRes.json()) as Post[];
+
+		const allCategories = Array.from(new Set(allPosts.flatMap((p) => p.categories)));
+
 		return {
 			postContent,
 			meta: { ...(post.metadata as Post), slug: params.slug },
+			allCategories,
 		};
 	} catch (err) {
 		throw redirect(301, '/posts');
