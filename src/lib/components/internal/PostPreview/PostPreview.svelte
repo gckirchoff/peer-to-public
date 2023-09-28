dd9693c904fbb22bd3b89c84352e4f0cd7b27e1a
gckirchoff <gckirchoff@gmail.com>
2023-09-28 18:10:15 -0400

    Displaying published state

--- src/lib/components/internal/PostPreview/PostPreview.svelte
+++ src/lib/components/internal/PostPreview/PostPreview.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+
 	import type Post from '$lib/types/post';
-	import { H4, SubTitle1, SubTitle2 } from '$lib/components/internal/typography';
+	import { Body1, H4, SubTitle1, SubTitle2 } from '$lib/components/internal/typography';
 	import CategoryTag from '../CategoryTag/CategoryTag.svelte';
 	import CategoryTagsList from '../CategoryTagsList/CategoryTagsList.svelte';
 
@@ -23,6 +25,15 @@
 				<SubTitle1>{post.description}</SubTitle1>
 				<SubTitle2>{post.date}</SubTitle2>
 			</div>
+			{#if dev}
+				<div class="published-state {post.published ? 'published' : 'draft'}">
+					<Body1>
+						<span>
+							{post.published ? 'Published' : 'Draft'}
+						</span>
+					</Body1>
+				</div>
+			{/if}
 		</a>
 	</article>
 </li>
@@ -32,11 +43,26 @@
 		display: grid;
 		grid-template-columns: 1fr 3fr;
 		gap: var(--spacing-32);
+		position: relative;
 
 		img {
 			object-fit: cover;
 			min-height: 100%;
 			width: 100%;
+		}
+
+		.published-state {
+			position: absolute;
+			right: 0;
+			padding: var(--spacing-4) var(--spacing-8);
+
+			&.published {
+				background-color: var(--clr-success-500);
+			}
+
+			&.draft {
+				background-color: var(--clr-warning-500);
+			}
 		}
 	}
 </style>
