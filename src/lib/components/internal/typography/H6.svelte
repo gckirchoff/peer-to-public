6a72a55bde893cbe19fff434c8eaa4de11a6f6b5
gckirchoff <gckirchoff@gmail.com>
2023-09-29 14:54:25 -0400

    Changing font weight for H6

--- src/lib/components/internal/typography/H6.svelte
+++ src/lib/components/internal/typography/H6.svelte
@@ -3,8 +3,8 @@
 <style lang="scss">
 	h6 {
 		font-size: var(--font-20);
-		font-weight: var(--font-weight-medium);
 		line-height: 1.6;
+		font-weight: var(--font-weight-regular);
 		font-family: var(--font-sans);
 		color: var(--clr-txt);
 	}
