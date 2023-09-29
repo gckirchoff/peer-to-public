9327c7cf299adee1b980445f6e45a7953756c4a7
gckirchoff <gckirchoff@gmail.com>
2023-09-29 15:39:05 -0400

    Removed fontweight from Body1

--- src/lib/components/internal/typography/Body1.svelte
+++ src/lib/components/internal/typography/Body1.svelte
@@ -3,7 +3,7 @@
 <style lang="scss">
 	p {
 		font-size: var(--font-20);
-		font-weight: var(--font-weight-regular);
+		// font-weight: var(--font-weight-regular);
 		line-height: 1.5;
 		font-family: var(--font-sans);
 		color: var(--clr-txt);
