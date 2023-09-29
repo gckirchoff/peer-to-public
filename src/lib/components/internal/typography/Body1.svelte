5696ed17f63d76d4877510948ca3db2ff4984aed
gckirchoff <gckirchoff@gmail.com>
2023-09-29 15:41:37 -0400

    Removing old style comment

--- src/lib/components/internal/typography/Body1.svelte
+++ src/lib/components/internal/typography/Body1.svelte
@@ -3,7 +3,6 @@
 <style lang="scss">
 	p {
 		font-size: var(--font-20);
-		// font-weight: var(--font-weight-regular);
 		line-height: 1.5;
 		font-family: var(--font-sans);
 		color: var(--clr-txt);
