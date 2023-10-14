369a1eb004bee612bb1ee92cc5d22697e1a02eb8
gckirchoff <gckirchoff@gmail.com>
2023-10-14 15:26:12 -0400

    Can disable button

--- src/lib/components/internal/Button/Button.svelte
+++ src/lib/components/internal/Button/Button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let type: 'submit' | 'button' | 'reset' | null | undefined = undefined;
 	export let style: string = '';
+	export let disabled = false;
 </script>
 
-<button on:click {type} {style}><slot /></button>
+<button on:click {type} {style} {disabled}><slot /></button>
 
 <style lang="scss">
 	button {
@@ -11,9 +12,15 @@
 		background-color: var(--clr-primary-300);
 		padding: var(--spacing-4) var(--spacing-8);
 		border-radius: var(--rounded-4);
+		transition: all 0.1s;
 
 		&:hover {
 			background-color: var(--clr-primary-400);
+		}
+
+		&:disabled {
+			filter: brightness(0.8);
+			pointer-events: none;
 		}
 	}
 </style>
