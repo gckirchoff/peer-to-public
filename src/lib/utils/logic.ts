0187c599cd8d615f92fbe0fafc56b6ad49d2246b
gckirchoff <gckirchoff@gmail.com>
2023-10-22 15:29:18 -0400

    Renamed function to capitalize

--- src/lib/utils/logic.ts
+++ src/lib/utils/logic.ts
@@ -37,5 +37,4 @@ export const getRandomInt = (min: number, max: number): number => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export const capitalizeFirstLetter = (text: string): string =>
-	text[0].toUpperCase() + text.slice(1);
+export const capitalize = (text: string): string => text[0].toUpperCase() + text.slice(1);
