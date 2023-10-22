5aef6f1bbe4239691649d2aadfb2a0a8069f6539
gckirchoff <gckirchoff@gmail.com>
2023-10-22 15:20:41 -0400

    Adding capitalize function

--- src/lib/utils/logic.ts
+++ src/lib/utils/logic.ts
@@ -36,3 +36,6 @@ export const getRandomInt = (min: number, max: number): number => {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const capitalizeFirstLetter = (text: string): string =>
+	text[0].toUpperCase() + text.slice(1);
