01b65e4d061ca0ee910e8ef6ea1c095c518c2b82
gckirchoff <gckirchoff@gmail.com>
2023-10-28 08:30:27 -0400

    Contact page has better styles

--- src/routes/(site)/contact/+page.svelte
+++ src/routes/(site)/contact/+page.svelte
@@ -1,8 +1,58 @@
 <script>
-	import Body1 from '$lib/components/internal/typography/Body1.svelte';
-	import H2 from '$lib/components/internal/typography/H2.svelte';
+	import { H3, Body1 } from '$lib/components/internal/typography';
+	import { Github, Twitter, Youtube } from '$lib/icons';
 </script>
 
-<H2>contact</H2>
+<div class="contact">
+	<H3>Contacts</H3>
 
-<Body1>Get in touch!</Body1>
+	<ul>
+		<li>
+			<Body1>
+				<a href="/contact" target="_blank" rel="noopener noreferrer">
+					<Youtube /> Youtube
+				</a>
+			</Body1>
+		</li>
+		<li>
+			<Body1>
+				<a href="/contact" target="_blank" rel="noopener noreferrer">
+					<Twitter /> Twitter
+				</a>
+			</Body1>
+		</li>
+		<li>
+			<Body1>
+				<a href="/contact" target="_blank" rel="noopener noreferrer">
+					<Github /> Github
+				</a>
+			</Body1>
+		</li>
+	</ul>
+</div>
+
+<style lang="scss">
+	@import '/src/styles/mixins.scss';
+	.contact {
+		@include respond('mobile') {
+			padding: var(--spacing-24);
+		}
+
+		ul {
+			list-style: none;
+
+			li {
+				display: flex;
+				align-items: center;
+				gap: 1rem;
+
+				a {
+					color: var(--clr-txt);
+					display: flex;
+					align-items: center;
+					gap: var(--spacing-8);
+				}
+			}
+		}
+	}
+</style>
