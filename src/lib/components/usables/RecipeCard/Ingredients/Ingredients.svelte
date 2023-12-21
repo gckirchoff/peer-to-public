a2a8532a54c41e306954f036c9cf8312f812cbea
gckirchoff <gckirchoff@gmail.com>
2023-12-21 10:05:26 -0500

    Extracted IngredientItem component

--- src/lib/components/usables/RecipeCard/Ingredients/Ingredients.svelte
+++ src/lib/components/usables/RecipeCard/Ingredients/Ingredients.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { H5, H6 } from '$lib/components/internal/typography';
 	import type { IngredientSection } from '../types';
+	import IngredientItem from './IngredientItem/IngredientItem.svelte';
 
 	export let allFoodItems: IngredientSection[];
 
@@ -20,19 +21,8 @@
 	{#each allFoodItems as { title, list }}
 		<ul>
 			<H6 style="margin-bottom: var(--spacing-8);">{title}</H6>
-			{#each list as { quantity, unit = '', item, note }}
-				<li class="ingredient-item">
-					<input id={item} type="checkbox" class="ingredient-checkbox" />
-					<label for={item} class="ingredient-label">
-						{quantity ? quantity * scale : ''}
-						{unit}
-						{item}
-						{#if note}
-							<span class="recipe-note">{note}</span>
-						{/if}
-					</label>
-					<span></span>
-				</li>
+			{#each list as ingredient}
+				<IngredientItem {ingredient} {scale} />
 			{/each}
 		</ul>
 	{/each}
@@ -61,59 +51,6 @@
 		grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
 		gap: var(--spacing-16);
 		padding: 0 0 var(--spacing-32) 0;
-
-		.ingredient-item {
-			position: relative;
-			margin-bottom: var(--spacing-8);
-
-			.ingredient-label {
-				font-size: 1.9rem;
-				cursor: pointer;
-				user-select: none;
-
-				&::before {
-					content: '';
-					display: inline-block;
-					width: 2rem;
-					height: 2rem;
-					border: 2px solid var(--clr-primary-500);
-					background-color: white;
-					border-radius: var(--rounded-4);
-					margin-right: 1rem;
-					vertical-align: middle;
-				}
-
-				&::after {
-					position: absolute;
-					top: 0.2rem;
-					left: 0.3rem;
-					content: '✓';
-					color: var(--clr-txt-neg);
-					display: none;
-				}
-
-				.recipe-note {
-					color: var(--clr-primary-700);
-					font-size: 1.7rem;
-				}
-			}
-		}
-
-		.ingredient-checkbox {
-			cursor: pointer;
-			user-select: none;
-			display: none;
-
-			&:checked + .ingredient-label {
-				&::before {
-					background-color: var(--clr-primary-500);
-				}
-
-				&::after {
-					display: block;
-				}
-			}
-		}
 	}
 
 	ul {
