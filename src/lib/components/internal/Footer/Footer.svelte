3ec7605cdaefa7aace5ea683dae8751a14a950de
gckirchoff <gckirchoff@gmail.com>
2023-12-27 12:32:36 -0500

    Adding Footer component

---
+++ src/lib/components/internal/Footer/Footer.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { navItems, siteConfig } from '$lib/config';
+	import NavItem from '../NavItem/NavItem.svelte';
+	import { Body2 } from '../typography';
+	import { footerHeight } from '../constants';
+</script>
+
+<footer style="--height: {footerHeight}">
+	<nav>
+		<ul>
+			{#each navItems as { route, title } (route)}
+				<NavItem href={route}>
+					<div class="nav-item">{title}</div>
+				</NavItem>
+			{/each}
+			<NavItem href="/rss.xml">RSS</NavItem>
+		</ul>
+	</nav>
+	<Body2>©{new Date().getFullYear()} {siteConfig.author}</Body2>
+</footer>
+
+<style lang="scss">
+	footer {
+		min-height: var(--height);
+		padding: var(--spacing-16) var(--spacing-24);
+		background-color: var(--clr-surface-300);
+		display: flex;
+		justify-content: space-between;
+
+		--flex-gap: var(--spacing-24);
+		--row-gap: var(--spacing-8);
+
+		nav {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			flex-wrap: wrap;
+			gap: var(--flex-gap);
+			row-gap: var(--row-gap);
+
+			ul {
+				display: flex;
+				align-items: center;
+				flex-wrap: wrap;
+				gap: var(--flex-gap);
+				row-gap: var(--row-gap);
+				list-style: none;
+			}
+		}
+	}
+</style>
