e51affe9595a909678cc197f4b71999d854c9559
gckirchoff <gckirchoff@gmail.com>
2023-12-27 14:44:32 -0500

    Updating config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,43 +3,31 @@ import { dev } from '$app/environment';
 // Main config for entire website
 export const siteConfig = {
 	title: 'Lucid Covid',
-	description: 'An Easy to Use Site Template',
+	description: 'Clear, visual explanations about SARS-CoV-2 and its effects',
 	url: 'https://your-blog-template.vercel.app',
-	link: 'https://github.com/gckirchoff/blog-template',
-	author: 'Amadeus Kirchoff',
+	link: 'https://github.com/gckirchoff/lucid-covid',
+	author: 'Gregory Kirchoff',
 	pages: {
 		landing: {
 			miniAboutMe: {
 				img: 'me.jpg',
 				quickDescription:
-					'My name is Gregory and I hope you enjoy this easy to use, but powerful site template!',
+					'We work with an international team of scientists and volunteers, united by the sole purpose of informing the public and government officials on the dangers of Covid-19',
 			},
 			welcome: {
-				header: 'Welcome to your site template!',
-				description: 'Create posts, photo galleries, and more!',
+				header: 'Lucid Covid',
+				description: 'Clear, visual explanations about SARS-CoV-2 and its effects',
 			},
 		},
 		about: {
 			img: 'me.jpg',
 			content: [
-				`Hi! My name is Gregory Kirchoff. I am a microbiology graduate with previous clinical
-			experience in emergency medicine from my time working on an ambulance. Since then, I
-			self-taught software development and now work as a software engineer.`,
-				`My goal when building this site template was to make something that is easily usable by
-				everyone, no matter their proficiency with computers, but also to be infinitely extendable
-				by those who do know how to code. All in just one payment. No subscriptions or premiums.
-				One payment, and you have everything you need to put your passion online.`,
-				`The magic behind all of this is how this template is architected. It is designed to be used
-				as a 'blog editing' app on your computer. When running this site locally, the website appears identical to
-				how it does now, but has admin pages for creating and customizing this site. All of the changes
-				you make there will change the source code of this application as if you were a software
-				developer writing the code yourself. Everything you create is saved in folders in the source code
-				as well.`,
-				`What all of this means is that you own everything. You own the tools for creating the posts and
-				changing the look and feel of your site. You own the "pseudo-database" that stores all of your posts.`,
-				`If this was run like other site building websites, you would need to pay a monthly subscription.
-				These payments go to renting their tools and reserving space on their databases. Instead,
-				it's all yours. Enjoy!`,
+				`SARS-CoV-2 is not the common cold and never will be. It is a SARS coronavirus. The implications of this
+				are major and require eradication to be the end-goal.`,
+				`The purpose of this site is to help the public and governing bodies to understand the importance of this goal.`,
+				`The posts and visualizations on this site are created through the collaberative efforts of numerous
+				scientists and volunteers.`,
+				`To reach out for further information or collaberation, please use the contact form to get in touch!`,
 			],
 		},
 		contact: {
@@ -54,7 +42,7 @@ export const postsPerPage = 10;
 // Edit this to alter the main nav menu. (Also used by the footer).
 export const navItems = [
 	{
-		title: 'Blog',
+		title: 'Posts',
 		route: '/posts',
 	},
 	// {
