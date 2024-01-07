c347011dd5c6870bf04ed25342f45320bd560ac6
Emma <eschaale@u.rochester.edu>
2024-01-07 18:27:06 -0500

    Update yaxis positioning and details

--- src/lib/components/internal/projects/CauseOfDeath/AxisY.svelte
+++ src/lib/components/internal/projects/CauseOfDeath/AxisY.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 	import type { ScaleBand } from 'd3-scale';
+	import type { BarData, Stage } from './constants';
+	import { getBarName } from './logic';
 
 	export let yScale: ScaleBand<string>;
+	export let bars: BarData[];
+	export let stage: Stage;
+
+	const sortedBars = bars.toSorted((a, b) => b.data.year2020 - a.data.year2020);
+
 	$: yLabels = yScale.domain();
 </script>
 
 <g class="axis y">
 	{#each yLabels as label, index}
 		<g class="tick" transform="translate(0, {yScale(label)})">
-			<text x={-10} y={0} text-anchor="end" dominant-baseline="middle">{label}</text>
+			<text x={-10} y={0} text-anchor="end" dominant-baseline="middle"
+				>{label} {' ' + (stage === "flattened" ? getBarName(sortedBars[index]) : '')}
+			</text>
 		</g>
 	{/each}
 </g>
