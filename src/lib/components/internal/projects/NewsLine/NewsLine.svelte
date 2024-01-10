c67e16b642bdbaccb6396037c2fceccdb60fa22f
gckirchoff <gckirchoff@gmail.com>
2024-01-09 21:52:22 -0500

    Using wastewater data instead of fake data

--- src/lib/components/internal/projects/NewsLine/NewsLine.svelte
+++ src/lib/components/internal/projects/NewsLine/NewsLine.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { csv } from 'd3';
 
 	import ScrollyTelly from './ScrollyTelly/ScrollyTelly.svelte';
 	import Chart from './Chart/Chart.svelte';
-	import { fakeData, type WastewaterReport } from './constants';
+	import type { WastewaterReport } from './constants';
+	import { parseRow } from './logic';
 
 	let data: WastewaterReport[] | null = null;
-	onMount(() => {
-		data = fakeData;
+	onMount(async () => {
+		data = await csv('/data/NewsLine/data.csv', parseRow);
 	});
 
 	let currentStep = 0;
 </script>
 
 {#if data}
-	<ScrollyTelly bind:currentStep steps={data.length} scrollHeight="50vh">
+	<ScrollyTelly bind:currentStep steps={5} scrollHeight="50vh">
 		<Chart {data} {currentStep} />
 	</ScrollyTelly>
 {:else}
