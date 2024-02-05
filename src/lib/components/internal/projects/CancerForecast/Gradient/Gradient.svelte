8f73f7b2dc5c9fe5b30211a90760115214dde157
gckirchoff <gckirchoff@gmail.com>
2024-02-05 09:13:20 -0500

    Added Gradient component

---
+++ src/lib/components/internal/projects/CancerForecast/Gradient/Gradient.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let id = '';
+	export let x1: number | string = 0;
+	export let x2: number | string;
+	export let y1: number | string = 0;
+	export let y2: number | string;
+	export let colors: string[] = [];
+</script>
+
+<linearGradient {id} {x1} {x2} {y1} {y2} gradientUnits="userSpaceOnUse" spreadMethod="pad">
+	{#each colors as color, i}
+		<stop offset={`${(i * 100) / (colors.length - 1)}%`} stop-color={color} />
+	{/each}
+</linearGradient>
