18cee91679c12bda4418d583c3c021d46cffbd31
gckirchoff <gckirchoff@gmail.com>
2024-02-05 09:17:21 -0500

    Added external link to mortality comparisons chart

---
+++ src/routes/(site)/external/mortality-comparison/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import ExternalLinks from '$lib/components/internal/ExternalLinks/ExternalLinks.svelte';
+	import CauseOfDeathChart from '$lib/components/internal/projects/CauseOfDeath/CauseOfDeathChart.svelte';
+</script>
+
+<CauseOfDeathChart />
+<ExternalLinks externalLink="https://www.google.com" internalLink="covid-as-a-top-cause-of-death" />
