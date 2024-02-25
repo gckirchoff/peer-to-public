249cfdd09378efbbcd5a53a1c664aaab98ef3159
gckirchoff <gckirchoff@gmail.com>
2024-02-25 10:45:59 -0500

    Adding redirect to tools directory and added tools list page

---
+++ src/routes/(site)/tools/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { H1, H4 } from '$lib/components/internal/typography';
+	import { links } from './constants';
+</script>
+
+<main>
+	<H1>Tools</H1>
+	<ul>
+		{#each links as link}
+			<li>
+				<H4>
+					<a href="/tools/{link.href}">{link.title}</a>
+				</H4>
+			</li>
+		{/each}
+	</ul>
+</main>
