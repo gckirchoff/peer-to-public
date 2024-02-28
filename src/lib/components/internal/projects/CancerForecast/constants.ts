3e387d2b857250014746383432a5e06fe45a999d
gckirchoff <gckirchoff@gmail.com>
2024-02-27 20:00:54 -0500

    Adding Audience type

--- src/lib/components/internal/projects/CancerForecast/constants.ts
+++ src/lib/components/internal/projects/CancerForecast/constants.ts
@@ -1,5 +1,3 @@
-export const baselineCancer = 18000000;
-
 export const beginningOfPandemic = new Date('2020/12/31');
 
 export const endOfChart = new Date('2090/05/12');
@@ -19,3 +17,4 @@ export interface Distribution {
 export type PreventionDeterminant = 'date' | 'panic';
 
 export type Mode = 'summed' | 'separate' | 'both';
+export type Audience = 'science' | 'general';
