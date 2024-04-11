c26d1788529f2f59b7611dc10f8cca0d9ea62723
gckirchoff <gckirchoff@gmail.com>
2024-04-11 16:36:42 -0400

    Added Tooltip

--- src/lib/components/internal/projects/CumulativeRisk/Chart/Chart.svelte
+++ src/lib/components/internal/projects/CumulativeRisk/Chart/Chart.svelte
@@ -6,6 +6,7 @@
 	import Line from './Line/Line.svelte';
 	import { margin, maxInfectionCount, type InfectionCumulativeRisk } from './constants';
 	import { getCumulativeRisks } from './logic';
+	import Tooltip from './Tooltip/Tooltip.svelte';
 
 	export let longCovidChance: number;
 
@@ -15,8 +16,7 @@
 	let width = 400;
 	let height = 400;
 
-	const statsCanadaData = getCumulativeRisks(0.15);
-	const cdcData = getCumulativeRisks(0.2);
+	const statsCanadaData = getCumulativeRisks(0.146);
 
 	$: innerChartWidth = width - margin.left - margin.right;
 	$: innerChartHeight = height - margin.top - margin.bottom;
@@ -28,16 +28,68 @@
 
 	$: xAccessorScaled = (d: InfectionCumulativeRisk) => xScale(xAccessor(d));
 	$: yAccessorScaled = (d: InfectionCumulativeRisk) => yScale(yAccessor(d));
+
+	let hoveredPoint: InfectionCumulativeRisk | null = null;
+
+	const updateHoveredData = (d: InfectionCumulativeRisk | null) => {
+		hoveredPoint = d;
+	};
 </script>
 
-<div class="chart-container" bind:clientWidth={width}>
+<div class="chart-container" bind:clientWidth={width} role="application">
 	<svg {width} {height}>
 		<g style="transform: translate({margin.left}px, {margin.top}px)">
 			<AxisX {xScale} {innerChartHeight} />
 			<AxisY {yScale} />
-			<Line data={cdcData} {xAccessorScaled} {yAccessorScaled} />
-			<Line data={statsCanadaData} {xAccessorScaled} {yAccessorScaled} />
+			<Line data={statsCanadaData} {xAccessorScaled} {yAccessorScaled} style="stroke: #ED1C24;" />
+			{#each statsCanadaData.slice(0, 4) as d}
+				<circle
+					cx={xAccessorScaled(d)}
+					cy={yAccessorScaled(d)}
+					fill="#ED1C24"
+					r={4}
+					on:mouseover={() => {
+						updateHoveredData(d);
+					}}
+					on:focus={() => updateHoveredData(d)}
+					on:mouseleave={() => updateHoveredData(null)}
+					role="tooltip"
+				/>
+			{/each}
 			<Line data={cumulativeRisks} {xAccessorScaled} {yAccessorScaled} />
+			{#each cumulativeRisks as d}
+				<circle
+					cx={xAccessorScaled(d)}
+					cy={yAccessorScaled(d)}
+					fill="purple"
+					r={4}
+					on:mouseover={() => {
+						updateHoveredData(d);
+					}}
+					on:focus={() => updateHoveredData(d)}
+					on:mouseleave={() => updateHoveredData(null)}
+					role="tooltip"
+				/>
+			{/each}
+			<g
+				style="transform: translate({xAccessorScaled(
+					statsCanadaData.at(-1) ?? { infectionCount: maxInfectionCount, risk: 0.8 },
+				) + 15}px, {yAccessorScaled(
+					statsCanadaData.at(-1) ?? { infectionCount: maxInfectionCount, risk: 0.8 },
+				) + 85}px);"
+			>
+				<text text-anchor="end" fill="#ED1C24">Forecast derived from</text>
+				<text text-anchor="end" dy={16} fill="#ED1C24">Statistics Canada</text>
+			</g>
 		</g>
 	</svg>
+	{#if hoveredPoint}
+		<Tooltip data={hoveredPoint} {xAccessorScaled} {yAccessorScaled} width={innerChartWidth} />
+	{/if}
 </div>
+
+<style lang="scss">
+	circle {
+		transition: all 0.3s ease-out;
+	}
+</style>
---
+++ src/lib/components/internal/projects/CumulativeRisk/Chart/Tooltip/Tooltip.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import type { ScaleBand, ScaleLinear } from 'd3';
+
+	import type { InfectionCumulativeRisk } from '../constants';
+	import { roundTo } from '../../logic';
+
+	export let data: InfectionCumulativeRisk;
+	export let xAccessorScaled: (d: InfectionCumulativeRisk) => number;
+	export let yAccessorScaled: (d: InfectionCumulativeRisk) => number;
+	export let width: number;
+
+	const xNudge = 15;
+	const yNudge = 15;
+
+	let tooltipWidth = 0;
+
+	$: x = xAccessorScaled(data);
+	$: y = yAccessorScaled(data);
+
+	$: isOverflowingRight = x + tooltipWidth + xNudge > width;
+	$: xPosition = isOverflowingRight ? x - tooltipWidth / 2 - xNudge : x + xNudge;
+	$: yPosition = y + yNudge;
+</script>
+
+<div
+	class="tooltip"
+	style="left: {xPosition}px; top: {yPosition}px; --max-width: {width}px"
+	bind:clientWidth={tooltipWidth}
+	in:fly={{ y: -20 }}
+	out:fly={{ y: 20 }}
+>
+	<h2 class="infection-count">{data.infectionCount} infections</h2>
+	<h3 class="risk">{roundTo(data.risk * 100, 2)}% cumulative risk</h3>
+</div>
+
+<style lang="scss">
+	.tooltip {
+		position: absolute;
+		background-color: white;
+		max-width: min(50%, var(--max-width));
+		padding: 8px 6px;
+		box-shadow: rgba(0, 0, 0, 0.15) 2px 3px 8px;
+		border-radius: 3px;
+		transition:
+			top 300ms ease,
+			left 300ms ease;
+		pointer-events: none;
+	}
+</style>
