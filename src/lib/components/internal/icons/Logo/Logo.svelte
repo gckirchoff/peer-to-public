97de2f678af8a5140d32c81d80d4dce82d79fabd
gckirchoff <gckirchoff@gmail.com>
2024-05-15 16:57:06 -0400

    Adding override prop

--- src/lib/components/internal/icons/Logo/Logo.svelte
+++ src/lib/components/internal/icons/Logo/Logo.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { theme } from '$lib/store';
+	import type { Theme } from '$lib/store';
 	import LightModeLogo from './LightModeLogo/LightModeLogo.svelte';
 	import DarkModeLogo from './DarkModeLogo/DarkModeLogo.svelte';
+
+	export let mode: Theme | undefined = undefined;
 </script>
 
-{#if $theme === 'light'}
+{#if mode === undefined}
+	{#if $theme === 'light'}
+		<LightModeLogo />
+	{:else}
+		<DarkModeLogo />
+	{/if}
+{:else if mode === 'light'}
 	<LightModeLogo />
 {:else}
 	<DarkModeLogo />
