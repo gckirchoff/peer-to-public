508a20d74d58a99b508c89735c5c5793424f897a
gckirchoff <gckirchoff@gmail.com>
2024-05-15 16:11:35 -0400

    Adding Logo

---
+++ src/lib/components/internal/icons/Logo/Logo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { theme } from '$lib/store';
+	import LightModeLogo from './LightModeLogo/LightModeLogo.svelte';
+	import DarkModeLogo from './DarkModeLogo/DarkModeLogo.svelte';
+</script>
+
+{#if $theme === 'light'}
+	<LightModeLogo />
+{:else}
+	<DarkModeLogo />
+{/if}
