1007d275041b26e61e1bf825c6f2e24578d7ebb1
gckirchoff <gckirchoff@gmail.com>
2024-05-18 10:35:31 -0400

    Improving legend

--- src/lib/components/internal/projects/CumulativeRisk/Chart/constants.ts
+++ src/lib/components/internal/projects/CumulativeRisk/Chart/constants.ts
@@ -2,10 +2,10 @@ export const margin = {
 	top: 15,
 	left: 50,
 	bottom: 45,
-	right: 125,
+	right: 180,
 };
 
-export const maxInfectionCount = 10;
+export const maxInfectionCount = 6;
 
 export type InfectionCumulativeRisk = {
 	infectionCount: number;
