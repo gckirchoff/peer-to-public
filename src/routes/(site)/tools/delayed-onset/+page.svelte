d7ab1fed62dd5becc1c8aa283a946cd932e3557f
gckirchoff <gckirchoff@gmail.com>
2024-06-12 07:03:26 -0400

    Adding exteneral links

--- src/routes/(site)/tools/delayed-onset/+page.svelte
+++ src/routes/(site)/tools/delayed-onset/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import ExternalLinks from '$lib/components/internal/ExternalLinks/ExternalLinks.svelte';
 	import CancerForecast from '$lib/components/internal/projects/CancerForecast/CancerForecast.svelte';
 </script>
 
 <CancerForecast />
+<ExternalLinks internalLink="the-danger-of-underestimating-delayed-onset-diseases" />
