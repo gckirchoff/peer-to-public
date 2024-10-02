646a2e7f9a4f463ce7abb8a4d539ff58e2bef375
gckirchoff <gckirchoff@gmail.com>
2024-10-01 21:19:55 -0400

    Improving info box styles

--- src/lib/components/internal/projects/CauseOfDeath/InfoBox/InfoBox.svelte
+++ src/lib/components/internal/projects/CauseOfDeath/InfoBox/InfoBox.svelte
@@ -11,7 +11,9 @@
 
 {#key infoBoxText}
 	<div class="info-box" in:fade={{ delay: 700 }} out:fade>
-		<Body1 style="color: var(--clr-text-on-surface-500);">{infoBoxContent[currentStep ?? 0]}</Body1>
+		<Body1 style="color: var(--clr-text-on-surface-500); font-size: var(--font-18);"
+			>{infoBoxContent[currentStep ?? 0]}
+		</Body1>
 	</div>
 {/key}
 
@@ -24,12 +26,9 @@
 		box-shadow: rgba(0, 0, 0, 0.15) 2px 3px 8px;
 		border-radius: 0.5rem;
 		pointer-events: none;
-		
-		padding: 2rem;
+
+		padding: var(--spacing-8) var(--spacing-16);
 		border-radius: 1rem;
 		background-color: var(--clr-surface-500);
-
-		
-		font-size: 1.8rem;
 	}
 </style>
