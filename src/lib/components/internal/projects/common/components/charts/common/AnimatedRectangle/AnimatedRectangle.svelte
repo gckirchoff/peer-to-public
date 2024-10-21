f62bf6b55e9c234ab05ca13dbef76dfc61b06b12
gckirchoff <gckirchoff@gmail.com>
2024-10-21 12:42:07 -0400

    Fixing animation

--- src/lib/components/internal/projects/common/components/charts/common/AnimatedRectangle/AnimatedRectangle.svelte
+++ src/lib/components/internal/projects/common/components/charts/common/AnimatedRectangle/AnimatedRectangle.svelte
@@ -6,6 +6,7 @@
 
 	let { x, y, width, height, ...rest }: AnimatedRectangleProps = $props();
 
+	let renders = $state(0);
 	let animated = tweened(
 		{
 			x,
@@ -19,6 +20,24 @@
 	);
 
 	$effect(() => {
+		// Hacky way to stop initial render
+		// having everything fly in from top left.
+		// Find a better way one day
+		if (renders < 2) {
+			animated.set(
+				{
+					x,
+					y,
+					width,
+					height,
+				},
+				{
+					duration: 0,
+				},
+			);
+			renders++;
+			return;
+		}
 		animated.set({
 			x,
 			y,
